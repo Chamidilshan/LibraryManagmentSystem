22ba94fef6fc5baca2ffe7c183dfb81d75511877
chathuransi <nayanatharachathuransi@gmail.com>
2023-09-11 15:39:41 +0530

    added home page

--- LibraryManagementSystem/App.xaml.cs
+++ LibraryManagementSystem/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace LibraryManagmentSystem
+namespace LibraryManagementSystem
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- LibraryManagementSystem/Properties/Settings.Designer.cs
+++ LibraryManagementSystem/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace LibraryManagmentSystem.Properties
+namespace LibraryManagementSystem.Properties
 {
 
 
